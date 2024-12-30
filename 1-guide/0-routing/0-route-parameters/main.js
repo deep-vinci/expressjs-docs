d003a28df29b828fde16b7c3e074d6d9b9516611
Deepak <deepakworkpc@gmail.com>
2024-12-31 01:20:39 +0530

    feat: params using :user :from-:to etc

---
+++ 1-guide/0-routing/0-route-parameters/main.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const port = 3000;
+app.get("/:user/", (req, res) => {
+    res.send(req.params);
+})
+app.get("/:user/:is_verified", (req, res) => {
+    res.send(req.params);
+})
+app.get("/:user/:is_verified/:from-:till", (req, res) => {
+    res.send(req.params);
+})
+
+
+app.listen(port);
